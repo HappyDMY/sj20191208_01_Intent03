fde4c532cfe50c653aebab4106adcf1cb472d5ae
jun <vagrantsj@gmail.com>
2019-12-08 10:33:31 +0900

    입력값을 원래 액티비티로 돌려보냄

--- app/src/main/java/com/tjoeun/sj20191208_01_intent03/EditNameActivity.kt
+++ app/src/main/java/com/tjoeun/sj20191208_01_intent03/EditNameActivity.kt
@@ -1,12 +1,31 @@
 package com.tjoeun.sj20191208_01_intent03
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_name.*
 
 class EditNameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_name)
+//
+        cancelBtn.setOnClickListener {
+            finish()
+        }
+
+        okBtn.setOnClickListener {
+            val inputName = nameEdt.text.toString()
+
+            val resultIntent =Intent()
+            resultIntent.putExtra("name", inputName)
+
+            setResult(Activity.RESULT_OK.resultIntent)
+
+
+        }
+
     }
 }
